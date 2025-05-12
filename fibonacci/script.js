a7037a30801bbe0f6d268d8714ab79724c175efe
a_osovsky <116560582+ArtemyOsovskoi@users.noreply.github.com>
2025-05-12 16:49:23 +0300

    clean up comments

--- fibonacci/script.js
+++ fibonacci/script.js
@@ -1,4 +1,3 @@
-/* command - node script.js */
 /* 
 Using iteration, write a function fibs which takes a number 
 and returns an array containing that many numbers from the Fibonacci sequence. 
@@ -47,44 +46,4 @@ function fibsRec(number) {
     return [...prevResult, nextNumber];
 }
 //console.log(fibsRec(8));
-
-/* 5 steps to solve recursion
-1) Simplest possible input (base case):
- number = 0
- result = [] (empty array)
- since quantity of fibonacci numbers in sequence is zero
-
- or number = 1
- result = [0]
- 1 number of fib sequence is [0] (it starts with 0 then 1, 1, 2, 3 etc.)
-
-2) Play around with examples and visualize:
-    fibResc(number) -> array.length == number;
-
-    fibResc(0) -> []
-    fibResc(1) -> [0]
-    fibResc(2) -> [0, 1] OR fibResc(number-1) + 1
-    fibResc(3) -> [0, 1, 1] OR fibResc(number-1) + 1
-    fibResc(4) -> [0, 1, 1, 2] OR fibResc(number-1) + 2
-    fibResc(5) -> [0, 1, 1, 2, 3] OR fibResc(number-1) + 3
-    fibResc(6) -> [0, 1, 1, 2, 3, 5] OR fibResc(number-1) + 5
-    fibResc(7) -> [0, 1, 1, 2, 3, 5, 8] OR fibResc(number-1) + 8 
-    fibResc(8) -> [0, 1, 1, 2, 3, 5, 8, 13] OR fibResc(number-1) + 13
-3) Relate hard cases to simpler cases:
-
-    each fibResc = fibResc(-1) + next number in fib sequence
-
-4)Generalise the pattern
-
-5) write code
-
-*/
-/* Задумался о двух подходах:
-
-1)Как должно быть - так и делаем
-            VS
-2)Как делаем - так и должно быть 
-1 предполагает влияние на что то своей волей
-2 предполагает принятие особенностей действия
-*/
 
